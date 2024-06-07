a1aab84469342990f9903a23f0594978c746967e
Anri Kezeroti <AKezeroti@abacusnext.com>
2024-06-07 19:24:26 +0400

    Add Testing Controller

---
+++ JWTDemo/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+            [HttpGet("TestAnonymous")]
+            public IActionResult GetTestAnonymous()
+            {
+                return Ok("Hello Anonymous");
+            }
+
+            [HttpGet("TestAuthorize")]
+            [Authorize]
+            public IActionResult GetTestAuthorize()
+            {
+                return Ok("Hello Authorize");
+            }
+      }
+}
